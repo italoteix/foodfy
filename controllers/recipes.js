1420c521716a354123301107eb420e6c70261253
Italo <italo_bruno91@hotmail.com>
2020-05-04 23:13:30 -0300

    :sparkles: feat: add index page

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const data = require('../data.json');
 
 exports.index = function(req, res) {
-  return res.render('admin/index')
+  return res.render('admin/index', { recipes: data.recipes });
 }
 
 exports.create = function(req, res) {
